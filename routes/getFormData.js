60b1e45720f395d29a170d01da3724281a2ed3b6
SAJID <sajid.munir4428@hotmail.com>
2024-06-14 19:54:45 +0500

    save form data route

--- routes/getFormData.js
+++ routes/getFormData.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const mongoString = process.env.DATABASE_URL;
 
-router.get('/api/getFormData', function(req, res, next) {
-    
+router.get('/api/getFormData', async function(req, res, next) {
+    const client = new MongoClient(mongoString);
+    let conn;
 
-    const data = '';
+     try {
+        conn = await client.connect();
+        let db = await conn.db('formData');
+        let collection = db.collection('');
+        let data = collection.findOne({ _id: req.body})
+        res.json({'data': result});
+     }
+     catch {
+        console.error(e);
+        res.json({'error': e});
+     }
 });
 
 module.exports = router;
--- routes/postFormData.js
+++ routes/postFormData.js
@@ -1,11 +1,26 @@
+const e = require('express');
 var express = require('express');
+const { MongoClient } = require('mongodb');
 var router = express.Router();
 require('dotenv').config();
 
 const mongoString = process.env.DATABASE_URL;
 
-router.post('/api/postFormData', function(req, res, next) {
-    
+router.post('/api/postFormData', async function(req, res, next) {
+    const client = new MongoClient(mongoString);
+    let conn;
+
+     try {
+        conn = await client.connect();
+        let db = await conn.db('formData');
+        let collection = db.collection('');
+        let result = await collection.insertOne(req.body);
+        res.json({'result': result});
+     }
+     catch {
+        console.error(e);
+        res.json({'error': e});
+     }
 });
 
 module.exports = router;
