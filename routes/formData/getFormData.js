247370ca1b9f43c174adbf6ede7d267b0981b68c
SAJID <sajid.munir4428@hotmail.com>
2024-06-24 12:05:16 +0500

    bug fixes

--- routes/formData/getFormData.js
+++ routes/formData/getFormData.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = import('express');
+const router = express.Router();
 const { MongoClient } = require('mongodb');
 const { mongoString, formDatabaseName, formCollectionName } = require('../../configuration');
 
