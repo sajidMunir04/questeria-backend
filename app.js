c306fc7c354d3df9bc2d52c3c95e425f8d12f22c
SAJID <sajid.munir4428@hotmail.com>
2024-06-26 11:28:00 +0500

    fix runtime issues

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-var passport = require('passport');
-
 
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.urlencoded({ extended: true }));
+// view engine setup
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.set('views', path.join(__dirname, 'views'));
 
-
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 var getFormDataRouter = require('./routes/formData/getFormData');
 var postFormDataRouter = require('./routes/formData/postFormData');
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(passport.authenticate('session'));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cookieParser());
+app.post('/',(req,res,next) => {
+  console.log('Received POST request:', req.body);
+  res.send("Hi, you are on homepage");
+  next();
+})
 
-
-app.use('/', indexRouter);
 app.use('/auth',authRouter);
 app.use('/users', usersRouter);
 app.use('/api',getFormDataRouter);
 app.use('/api',postFormDataRouter);
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
