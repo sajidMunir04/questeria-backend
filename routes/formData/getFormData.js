bbcd404485b63aa2d5f146ad27a01f054063cbe0
SAJID <sajid.munir4428@hotmail.com>
2024-06-30 17:12:31 +0500

    fix get Form Data error

--- routes/formData/getFormData.js
+++ routes/formData/getFormData.js
@@ -7,15 +7,14 @@ router.get('/getFormData', async function(req, res, next) {
    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    const client = new MongoClient(mongoString);
-    let conn;
 
      try {
-        conn = await client.connect();
-        let db = conn.db(formDatabaseName);
-        let collection = db.collection(formCollectionName);
-        let data = collection.findOne({});
-        res.json({'data' : data});
+         const client = new MongoClient(mongoString);
+         const conn = await client.connect();
+         const db = conn.db(formDatabaseName);
+         const collection = db.collection(formCollectionName);
+         const data = await collection.findOne({});
+         res.json(data);
      }
      catch {
         console.error(e);
