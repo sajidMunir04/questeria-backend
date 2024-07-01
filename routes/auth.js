c4bafdceea642dec370900a83d789bd407e866ed
SAJID <sajid.munir4428@hotmail.com>
2024-07-01 12:20:54 +0500

    fix PSQL bug

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,7 @@ const { userDatabaseLink } = require('../configuration');
 const db = pgp(userDatabaseLink);
 
 passport.use(new LocalStrategy(function verify(username,password,cb) {
-    db.one('Select user from Users WHERE first_name = $1',[username],function(err,user) {
+    db.none('Select user from Users WHERE first_name = $1',[username],function(err,user) {
         if (err) {
             return cb(err);
         }
